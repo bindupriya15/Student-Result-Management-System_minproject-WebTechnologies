851a3fbf7d0b21b6e9bb855bc6f3c35e6d96a689
S.BINDUPRIYA <127580053+bindupriya15@users.noreply.github.com>
2023-11-15 22:09:47 +0530

    Add files via upload

---
+++ Student Result Management System Project/Logout.php
@@ -0,0 +1,9 @@
+<?php
+ session_start();
+ if(session_destroy()) {
+ header("Location: login.php");
+ echo '<script language="javascript">';
+ echo 'alert("Logout successful")';
+ echo '</script>';
+ }
+?>
